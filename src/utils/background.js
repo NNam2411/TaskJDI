764ffbabfeefb4dc5fbcee7af5ec7e6e6de7039c
NNam2411 <nguyennam2002ro@gmail.com>
2023-09-22 13:59:32 +0700

    Get Balance of Network and Check Network

---
+++ src/utils/background.js
@@ -0,0 +1,7 @@
+// // background.js
+// chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+//   if (message === "extension_changed") {
+//     // Xử lý khi extension thay đổi
+//     console.log("Extension thay đổi");
+//   }
+// });
--- src/utils/etherUtils.js
+++ src/utils/etherUtils.js
@@ -1,21 +1,52 @@
 import { ethers } from "ethers";
 
-const connectToMetaMask = async () => {
+export const connectToMetaMask = async () => {
   try {
-    if (window.ethereum) {
-      const provider = new ethers.BrowserProvider(window.ethereum);
-      await window.ethereum.enable();
-      const signer = provider.getSigner();
-      return signer;
-    } else {
+    if (!window.ethereum) {
       throw new Error(
         "Metamask not detected. Please install and connect Metamask."
       );
+    }
+    if (window.ethereum) {
+      const provider = new ethers.BrowserProvider(window.ethereum);
+      await provider.send("eth_requestAccounts", []);
+      // Lấy tài khoản
+      const signer = provider.getSigner();
+      return signer;
     }
   } catch (error) {
     console.error("Error connecting to MetaMask:", error);
     throw error;
   }
+};
+
+export const CheckNetwork = async () => {
+  const provider = new ethers.BrowserProvider(window.ethereum);
+  const network = await provider.getNetwork();
+  console.log(
+    "file: etherUtils.js:25|| connectToMetaMask || network:",
+    network
+  );
+  const networkId = network.chainId;
+  const networkName = network.name;
+  if (networkId === 97n && networkName === "bnbt") {
+    console.log("Ethers BSC succes:", "Connected to BSC Testnet");
+    return true;
+  } else {
+    console.log("Ethers BSC warning:", "Please connected to BSC Testnet");
+    return false;
+  }
+};
+export const CheckBalance = async (address) => {
+  const provider = new ethers.BrowserProvider(window.ethereum);
+  const balance = await provider.getBalance(address);
+
+  console.log("file: etherUtils.js:43 || CheckBalance || balance:", balance);
+  console.log(
+    "file: etherUtils.js:44 || CheckBalance || balance:",
+    typeof balance
+  );
+  return balance;
 };
 
 export default connectToMetaMask;
